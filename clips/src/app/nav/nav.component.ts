ad1055e45fd5d719515688e01d3dc937a6300b61
emil19ro <emil19ro@gmail.com>
2023-07-11 01:25:29 +0200

    feat(AuthService): adding log in and log out feature

--- clips/src/app/nav/nav.component.ts
+++ clips/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -11,13 +12,19 @@ export class NavComponent {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService) {}
+    public auth: AuthService,
+    private afAuth: AngularFireAuth) {}
 
   openModal($event: Event) {
     $event.preventDefault(); // this i here in order to prevent anchor from html template to add the
     //link and do a redirect, in our case be have an diez # there
     // here we call the function in order to toggle the boolean on and off
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event){
+    $event.preventDefault();
+    await this.afAuth.signOut();
   }
 
 }
